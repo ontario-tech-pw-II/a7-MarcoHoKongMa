e098cc8b7114c29b32d2cefbf2bfe00a38b3f819
60004908 <marcohokong.ma@ontariotechu.net>
2020-02-28 10:41:18 -0500

    commit on part2

--- Part-2/CheckingAccount.cpp
+++ Part-2/CheckingAccount.cpp
@@ -7,27 +7,48 @@ using namespace std;
 CheckingAccount::CheckingAccount( double initialBalance, double fee ) : Account( initialBalance ) 
 {
 	// your code
+	if(fee < 0){
+		fee = 0;
+	}
+	transactionFee = fee;
+	setBalance(initialBalance);
 } 
 
 // credit (add) an amount to the account balance and charge fee
 void CheckingAccount::credit( double amount )
 {
 	// your code
+	setBalance(getBalance() + amount);
+	chargeFee();
 } 
 
 // debit (subtract) an amount from the account balance and charge fee
 bool CheckingAccount::debit( double amount )
 {
 	// your code
+	if (amount > getBalance()){
+		return false;
+	} else{
+		setBalance(getBalance() - amount);
+		chargeFee();
+		return true;
+	}
 }
 
 // subtract transaction fee
 void CheckingAccount::chargeFee()
 {
 	// your code
+	double b;
+	b = getBalance() - transactionFee;
+	setBalance(b);
 }
 
 void CheckingAccount::display(ostream & os) const
 {
 	// your code
+	os.precision(2);
+	os << "Account type: Checking" << endl;
+	os << "Balance: $ " << fixed << getBalance() << endl;
+	os << "Interest Rate (%): " << fixed << transactionFee << endl;
 }
--- Part-2/SavingsAccount.cpp
+++ Part-2/SavingsAccount.cpp
@@ -6,12 +6,24 @@ using namespace std;
 SavingsAccount::SavingsAccount( double initialBalance, double rate ) : Account( initialBalance ) 
 {
 	// your code
+	if(rate < 0){
+		rate = 0;
+	}
+	interestRate = rate;
+	setBalance(initialBalance);
 }
 
 double SavingsAccount::calculateInterest() {
 	// your code
+	double b;
+	b = getBalance() * interestRate;
+	return b;
 } 
 void SavingsAccount::display(ostream & os) const
 {
 	// your code
+	os.precision(2);
+	os << "Account type: Saving" << endl;
+	os << "Balance: $ " << fixed << getBalance() << endl;
+	os << "Interest Rate (%): " << fixed << interestRate * 100 << endl;
 }
